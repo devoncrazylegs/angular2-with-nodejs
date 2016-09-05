41c6a46a26282ca48fe9db9915d1700de89d1563
James Holman <james.alex.holman@gmail.com>
2016-09-05 13:06:50 +0100

    added more routes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var apiRoutes = require('./routes/api');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static('client'));
 app.use(express.static(path.join(__dirname, '/client/assets')));
 
 app.use('/', routes);
+app.use('/api', apiRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
